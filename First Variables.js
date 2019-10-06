807a346b354b89fa5fefe62f43d48db4611eba60
JulienGoMo <56203259+JulienGoMo@users.noreply.github.com>
2019-10-06 16:22:33 -0400

    Add files via upload

---
+++ First Variables.js
@@ -0,0 +1,19 @@
+/*Learning varibales
+creattion of the variable*/
+
+//Creation of the variable juilen
+let julien;
+
+//Assigning a value to the variable Julien
+julien=("my name is Julien" + " ,and I'm 35. ");
+
+//Creation of the variable cats, and assigning its value at the same time
+let cat=("I have two cats named Taco and Puffin.");
+
+//Creation of a constant variable
+const penelope=("My wife's name is Penelope!");
+
+
+//Display the variables
+console.log(julien+cat);
+console.log (penelope);
