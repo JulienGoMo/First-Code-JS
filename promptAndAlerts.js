3dfc7d171852d5f7a8500948608e4a2956b1625f
JulienGoMo <56203259+JulienGoMo@users.noreply.github.com>
2019-10-09 18:15:58 -0400

    Add files via upload
    My exercices at the date: 10/9/19

---
+++ promptAndAlerts.js
@@ -0,0 +1,10 @@
+//prompt and alerts
+const num1 = Number(prompt('Enter a number: '));
+const num2 = Number(prompt('Enter a second number'));
+const total = num1*num2;
+alert (num1 +' times ' + num2 +' equals ' + total);
+
+let cat1 = String(prompt('what is the name of your first cat?'));
+let cat2 = String(prompt('What is the name of your second cat?'));
+const tac = String(cat1 + cat2);
+alert ('Your only cat\'s name is '+ tac);
