c1806898eefb3d933ce905df62a8c5beac58b7e0
JulienGoMo <56203259+JulienGoMo@users.noreply.github.com>
2019-10-20 09:27:56 -0400

    Add files via upload

---
+++ if else experiment 2.js
@@ -0,0 +1,18 @@
+const stupid = ("no no no! I said between 1 and 10!");
+
+let number = prompt("pick a number between 1 and 10");
+
+if ((number >=1) &&(number <= 10)) {
+    alert ("I'm going to add 200% to your number");
+} else (alert (stupid));
+
+number *= 3;
+
+const resEven = ("your number is " + (number) + ", and is an even number");
+const resOdd = ("your number is " + (number) + ", and is an odd number");
+
+if (number==1,3,5,7,9,11,13,15,17,19,21,23,25,27,29){
+    alert (resOdd);
+} else (alert(resEven));
+
+alert(number);
